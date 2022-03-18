e8822af73aa99c9c63fa4bf9ca3b1769fc2fc233
thomasalves11 <thomas.alves@agisrn.com>
2022-03-18 12:52:39 -0300

    ajustes prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   public_Key: '38610a3dfe5d1bb4758924e29798a5fa',
-  hash: '78cc56b83e445fb309b64ef1db417783',
-  url: `https://gateway.marvel.com/v1/public/characters`,
+  hash: '5446a679cdc4eb0b76c5241eac819e8c',
+  url: `https://gateway.marvel.com:443/v1/public/characters`,
 };
