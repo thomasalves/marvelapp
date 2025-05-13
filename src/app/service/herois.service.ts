6f23a1d727425fd54ff03732d286fb35a7d06a6b
Thomas Vinícius de Araújo Alves <32557796+thomasalves@users.noreply.github.com>
2025-05-13 16:08:11 -0300

    Update herois.service.ts

--- src/app/service/herois.service.ts
+++ src/app/service/herois.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from  '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map }from 'rxjs/operators';
-import { environment } from '../../environments/environment'
+
 
 
 @Injectable({
@@ -14,12 +14,12 @@ export class HeroisService {
   constructor(private http: HttpClient) { }
 
  public  GetAll(): Observable<Api> {
-    return  this.http.get<Api>(`${environment.url}?ts=1647618227&apikey=${environment.public_Key}&hash=${environment.hash}&limit=100` )
+    return  this.http.get<Api>(`${environment.urlGetway}?ts=1647618227&apikey=${environment.public_Key}&hash=${environment.hash}&limit=100` )
       .pipe(map((data): Api => data))
   }
 
  public getCharacterByName(name: string) :Observable<Api> {
-    let requestUrl = `${environment.url}?orderBy=name&nameStartsWith=${name}&ts=1647618227&apikey=${environment.public_Key}&hash=${environment.hash}`;
+    let requestUrl = `${environment.urlGetway}?orderBy=name&nameStartsWith=${name}&ts=1647618227&apikey=${environment.public_Key}&hash=${environment.hash}`;
     return this.http.get<Api>(requestUrl)
       .pipe(map((data): Api => data));
 }
