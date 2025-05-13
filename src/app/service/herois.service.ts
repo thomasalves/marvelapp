27fbd84dd4afdc6748970f2afd88543bb47050ff
Thomas Vinícius de Araújo Alves <thomasalves1111@gmail.com>
2025-05-13 18:30:35 -0300

    ajuste de auth

--- src/app/service/herois.service.ts
+++ src/app/service/herois.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from  '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map }from 'rxjs/operators';
-import { environment } from '../..//environments/environment';
-
+import { environment } from '../../environments/environment';
+import * as CryptoJS from 'crypto-js'; 
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +13,34 @@ export class HeroisService {
 
   constructor(private http: HttpClient) { }
 
-  public  GetAll(): Observable<Api> {
-    return  this.http.get<Api>(`${environment.urlGetway}?ts=1647618227&apikey=${environment.public_Key}&hash=${environment.hash}&limit=100` )
-      .pipe(map((data): Api => data))
-  }
+public GetAll(): Observable<Api> {
+  const timestamp = new Date().getTime().toString(); 
+  const apiKey = environment.public_Key; 
+  const privateKey = environment.private_Key;  
+
+  const hash = CryptoJS.MD5(timestamp + privateKey + apiKey).toString(CryptoJS.enc.Hex);
+
+
+  // Set up the parameters
+  const params = {
+    ts: timestamp,
+    apikey: apiKey,
+    hash: hash,
+  };
+  return this.http.get<Api>(`${environment.urlGetway}`, {
+    params,
+  }).pipe(map((data): Api => data));
+}
 
   public getCharacterByName(name: string) :Observable<Api> {
-    let requestUrl = `${environment.urlGetway}?orderBy=name&nameStartsWith=${name}&ts=1647618227&apikey=${environment.public_Key}&hash=${environment.hash}`;
+    const timestamp = new Date().getTime().toString(); 
+    const apiKey = environment.public_Key; 
+    const privateKey = environment.private_Key;  
+
+    const hash = CryptoJS.MD5(timestamp + privateKey + apiKey).toString(CryptoJS.enc.Hex);
+
+    let requestUrl = `${environment.urlGetway}?orderBy=name&nameStartsWith=${name}&ts=1647618227&apikey=${environment.public_Key}&hash=${hash}`;
     return this.http.get<Api>(requestUrl)
-      .pipe(map((data): Api => data));
-}
+        .pipe(map((data): Api => data));
+  }
 }
